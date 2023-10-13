209f12ce7000c614f1f0285b3a7cba8040f9977a
juanmmartinss <juanmmartinss@outlook.com>
2023-10-13 16:24:58 -0300

    primeiro teste

--- Projeto/main.c
+++ Projeto/main.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
+    
 
     if (argc != 2) {//verifica se o arquivo foi passado
         fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
